75013fb5d75303af34b6bf4a6aeea67f82d6f785
Ranu Singhal <ranu1996@gmail.com>
2018-01-26 13:27:35 +0530

    Changes in author.

--- src/main/java/ranu/springframework/spring5webapp/Author.java
+++ src/main/java/ranu/springframework/spring5webapp/Author.java
@@ -13,6 +13,7 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+    private String hello;
 
     @ManyToMany(mappedBy="authors")
     private Set<Book> books=new HashSet<>();
